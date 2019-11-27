6553cf1940aeeb7698736973822dfce2ae224df7
sriram <ggcsriram@gmail.com>
2019-11-27 10:33:16 +0530

    added rotating container in signup

--- login/lib/src/signup_page.dart
+++ login/lib/src/signup_page.dart
@@ -10,16 +10,43 @@ class Signup extends StatefulWidget {
   _SignupState createState() => _SignupState();
 }
 
-class _SignupState extends State<Signup> {
+class _SignupState extends State<Signup> with SingleTickerProviderStateMixin{
+  Animation animation;
+  AnimationController animationController; 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+   animationController=AnimationController(vsync: this,duration: Duration(seconds: 2)) ;
+   animation=Tween<double>(begin: 0.0,end: 360.0).animate(animationController);
+   animationController.repeat();
+    
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: Center(
-            child: RaisedButton(onPressed: () {
-              widget.onSuccess('123');
-            },
-        child: Text('hello'),
-      ),
+          body: Stack(
+            children: <Widget>[
+              Center(
+                child: AnimatedBuilder(
+                  
+                                  child: Container(
+                    height: 300,
+                    width: 300,
+                    color: Colors.orangeAccent,
+                    ),  animation: animation, builder: (BuildContext context, Widget child) {
+                      return Transform.rotate(child: child, angle: animation.value,);
+                    },
+                )
+              ),
+              Center(child: Container(
+                height: 150,
+                width: 150,
+                color: Colors.pink,
+              
+                ))
+            ],
           ),
     );
   }
